4ecc083ae785a6be8cc388c3a238844a991e929c
PraptiRijal84 <153342111+prapti48@users.noreply.github.com>
2024-03-15 12:37:27 +0545

    Add files via upload

---
+++ print.py
@@ -0,0 +1,16 @@
+x = "Prapti"
+y = "Rijal"
+print(f"{x} {y}")
+
+'''import function'''
+#print("Your targeting " + input("What ip would you target ?"))
+
+x = "5"
+y = 7
+#print(int(x)+y)
+
+first_name = input("What is your first name ?")
+last_name = input("What is your last name ? ")
+#print(f"Your name is {first_name} {last_name}")
+
+
